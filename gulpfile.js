9be72c47bd35d639ab35c1a96d044fbee2a0029a
Robert Clark <robertclark0@gmail.com>
2016-06-17 11:08:46 -0500

    Init

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+﻿// Include gulp
+var gulp = require('gulp');
+// Include plugins
+var concat = require('gulp-concat');
+var order = require('gulp-order');
+// Concatenate JS Files
+gulp.task('scripts-libs', function () {
+    return gulp.src([
+        'libraries/jquery/jquery-1.12.3.min.js',
+        'libraries/angular/angular.min.js',
+        'libraries/angular/angular-material.min.js',
+        'libraries/angular/angular-sanitize.min.js',
+        'libraries/angular/angular-animate.min.js',
+        'libraries/angular/angular-aria.min.js',
+        'libraries/ui-router/angular-ui-router.min.js',
+        'libraries/ngStorage/ngStorage.min.js'
+    ])
+      .pipe(concat('libraries.min.js'))
+      .pipe(gulp.dest('assets/js'));
+});
+
+gulp.task('scripts-app', function () {
+    return gulp.src([
+        'assets/js/app.module.js',
+        'assets/js/app.config.js',
+        'core-components/**/*.js',
+        'shared-components/**/*.js'
+    ])
+      .pipe(order([
+          'app.module.js',
+          '**/module.js'
+      ]))
+      .pipe(concat('dist.js'))
+      .pipe(gulp.dest('assets/js'));
+});
+
+gulp.task('css-app', function () {
+    return gulp.src([
+        'libraries/bootstrap/bootstrap.min.css',
+        'libraries/angular/angular-material.min.css',
+        'assets/css/style.css',
+        'core-components/**/*.css',
+        'shared-components/**/*.css'
+    ])
+      .pipe(concat('dist.css'))
+      .pipe(gulp.dest('assets/css'));
+});
+
+
+// Default Task
+gulp.task('default', ['scripts-libs', 'scripts-app', 'css-app']);
---
+++ readme.txt
@@ -0,0 +1,8 @@
+Project Structure:
+
+Deloy:
+
+-assets
+-core-components /less js files
+-shared-components /less js files
+-index.html
